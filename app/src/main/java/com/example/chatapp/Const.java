1d0db530016f6f5635bd22124ff8d0170672aaa9
block2k <longbdhe140529@fpt.edu.vn>
2022-01-21 11:05:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chatapp/Const.java
@@ -0,0 +1,7 @@
+package com.example.chatapp;
+
+public class Const {
+    public static String TEXT_REQUIRED_ALL_FIELDS = "All fields are required";
+    public static String WARNING_LOGIN_FAIL = "Authentication failed!";
+    public static String LOGIN_SUCCESS = "Login success!";
+}
---
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.chatapp.fragments.ChatsFragment;
+import com.example.chatapp.fragments.UsersFragment;
+import com.example.chatapp.model.User;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity {
+    CircleImageView profile_image;
+    TextView username_main;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindingView();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                username_main.setText(user.getUsername() == null ? "Can't get username" : user.getUsername());
+                if ("default".equals(user.getImageURL())) {
+                    profile_image.setImageResource(R.mipmap.ic_launcher);
+                } else {
+                    Glide.with(MainActivity.this).load(user.getImageURL()).into(profile_image);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager viewPager = findViewById(R.id.view_pager);
+
+        ViewPageAdapter viewPageAdapter = new ViewPageAdapter(getSupportFragmentManager());
+
+        viewPageAdapter.addFragment(new ChatsFragment(), "Chats");
+        viewPageAdapter.addFragment(new UsersFragment(), "Users");
+
+        viewPager.setAdapter(viewPageAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    //add option menu to toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            //click button logout
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
+                finish();
+                return true;
+        }
+
+        return false;
+    }
+
+    private void bindingView() {
+        profile_image = findViewById(R.id.profile_image);
+        username_main = findViewById(R.id.username_main);
+    }
+
+    class ViewPageAdapter extends FragmentPagerAdapter {
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        ViewPageAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+    }
+}
+
